7cb3e0583ad314bf867f0858935eabfb16b99ab0
INARI0123 <43335288+INARI0123@users.noreply.github.com>
2018-09-18 17:06:45 +0800

    Add files via upload

---
+++ w02/Input a seconds.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int hh,mm,ss;
+	int sec;
+	printf("Enter seconds: ");
+	scanf("%d",&sec);
+	hh =sec/3600;
+	mm =(sec%3600)/60;
+	ss =sec%60;
+	printf("%d seconds =%02d : %02d : %02d \n",sec ,hh ,mm ,ss);
+	
+	
+}
